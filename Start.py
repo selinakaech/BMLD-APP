3d87d71a3a0b34e42fc220b02ea4add950d75101
selinakaech <kaechsel@students.zhaw.ch>
2025-04-19 15:55:11 +0200

    Login Manager angepasst

--- Start.py
+++ Start.py
@@ -3,7 +3,7 @@ from utils.data_manager import DataManager
 from utils.login_manager import LoginManager
  
 # initialize the data manager
-data_manager = DataManager(fs_protocol='webdav', fs_root_folder="LN1_Demo_App")  # switch drive
+data_manager = DataManager(fs_protocol='webdav', fs_root_folder="BMLD_Daten")  # switch drive
  
 # initialize the login manager
 login_manager = LoginManager(data_manager)
