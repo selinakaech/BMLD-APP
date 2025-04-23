c8d03994708f8502845957687bb9268e704ecde4
SorayaNoreen <gfrersor@students.zhaw.ch>
2025-04-23 12:56:37 +0200

    .py

--- pages/Hauptseite.py
+++ pages/Hauptseite.py
@@ -67,7 +67,7 @@ st.markdown("<hr>", unsafe_allow_html=True)
  
 # Seiten-Setup
 seiten = {
-    "🧪 Konzentrationen": "Konzentrationen",
+    "🧪 Konzentrationen": "Konzentrationen.py",
     "💧 Lösungen": "Lösungen",
     "⚖️ Massenrechner": "Massenrechner",
     "🔬 Periodensystem": "Periodensystem",
