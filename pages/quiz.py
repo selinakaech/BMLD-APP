b0b96085ee866c0e6ce8fff8e8f987b74414104d
SorayaNoreen <gfrersor@students.zhaw.ch>
2025-04-23 14:08:01 +0200

    rechner fehler angepasst

--- pages/quiz.py
+++ pages/quiz.py
@@ -1,8 +1,3 @@
-# ====== Start Login Block ======
-from utils.login_manager import LoginManager
-LoginManager().go_to_login('Start.py') 
-# ====== End Login Block ======
-
 import streamlit as st
 import pandas as pd
 import numpy as np
--- pages/tagebuch.py
+++ pages/tagebuch.py
@@ -1,6 +1,1 @@
-# ====== Start Login Block ======
-from utils.login_manager import LoginManager
-LoginManager().go_to_login('Start.py') 
-# ====== End Login Block ======
-
 import streamlit as st
