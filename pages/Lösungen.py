4086876303f45169f4cc8d9ec7e5c7b477204c9d
SorayaNoreen <gfrersor@students.zhaw.ch>
2025-04-23 09:51:59 +0200

    login aktualisiert

--- pages/Lösungen.py
+++ pages/Lösungen.py
@@ -1,11 +1,6 @@
 # ====== Start Login Block ======
 from utils.login_manager import LoginManager
-import streamlit as st
-
-# Überprüfen, ob der Benutzer eingeloggt ist
-if "logged_in" not in st.session_state or not st.session_state.logged_in:
-    LoginManager().go_to_login('Start.py')
-    st.stop()  # Stoppt die Ausführung, bis der Benutzer eingeloggt ist
+LoginManager().go_to_login('Start.py') 
 # ====== End Login Block ======
 
 
